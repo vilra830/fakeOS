cf75bc942f22f67fb37eda2c3c7014630d2556a5
vilra830 <villafuerte.rhea@gmail.com>
2024-12-30 21:12:54 +1100

    adds start button functionality on toggling start menu

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const timestamp = document.querySelector("#timestamp");
+const startMenu = document.querySelector("#start-menu");
+const startButton = document.querySelector("#start-button");
 
 //display time
 const timeStamp = () => {
@@ -12,9 +14,22 @@ const timeStamp = () => {
   if (hour >= 12) {
     am = "PM";
   }
+  if (minute < 10) {
+    minute = "0" + minute;
+  }
 
   timestamp.innerHTML = `${hour}:${minute} ${am}`;
   //   timestamp.innerHTML = `${date}`;
 };
-
 timeStamp();
+
+//toggle start menu
+startButton.addEventListener("click", (ev) => {
+  console.log("hello");
+  ev.preventDefault();
+  if (startMenu.style.display === "none") {
+    startMenu.style.display = "block";
+  } else {
+    startMenu.style.display = "none";
+  }
+});
